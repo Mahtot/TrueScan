ed5c54dc754546923ba8e304d40d72d2504be6e7
[mahtot] <mahtotgher13@gmail.com>
2025-05-14 10:50:00 +0300

    Fixed errors

--- src/main/java/com/truescan/truescan_backend/config/WebConfig.java
+++ src/main/java/com/truescan/truescan_backend/config/WebConfig.java
@@ -13,10 +13,13 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") //  frontend URL
+                        .allowedOriginPatterns("http://localhost:3000") //  frontend URL
                         .allowedMethods("*") //  ["GET", "POST", "PUT", "DELETE"]
                         .allowedHeaders("*")
                         .allowCredentials(true);
+
+
+
             }
         };
     }
